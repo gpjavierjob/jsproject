4d6e489c73791552a74ad1b317ae06265bc35f1e
Javier Gómez Pérez <gpjavierjob@gmail.com>
2024-10-10 16:15:21 -0300

    Adicionada la clase AlmacenamientoLocal y ajustadas las clases Carrito, CarritoLinea, Catalogo y Producto para su uso.

--- js/clases.js
+++ js/clases.js
@@ -11,6 +11,40 @@ const TITULO_IMPORTE = "Importe";
 const TITULO_CON_IVA = "Con IVA";
 const TITULO_A_PAGAR = "A pagar";
 
+const KEY_CATALOGO = "CATALOGO_PRODUCTOS";
+const KEY_CARRITO = "CARRITO_COMPRAS";
+
+class AlmacenamientoLocal {
+    // Almacena un mapa cuyos valores son objetos
+    static guardarMapa (key, mapa) {
+        // Se convierte el contenido del mapa a un arreglo, se serializa y se almacena.
+        localStorage.setItem(key, JSON.stringify(Array.from(mapa.entries())));
+    }
+
+    // Construye y devuelve un mapa cuyos valores son objetos. Si se desea recuperar el tipo
+    // de los objetos, se debe proporcionar una función factoría que devuelve el objeto del
+    // tipo correcto. Esta función debe esperar como único parámetro el objeto almacenado.
+    static obtenerMapa (key, funcionFactoria) {
+        // Obtener el arreglo del almacenamiento local
+        const arreglo = JSON.parse(localStorage.getItem(key) ?? "[]");
+        // Si no se proporciona la función factoría se crea el mapa con el arreglo obtenido
+        // del localStorage y se devuelve.
+        if (funcionFactoria === undefined || funcionFactoria === null) 
+            return new Map(arreglo);
+        // Se debe recuperar el tipo de los objetos antes de crear el objeto Map. Para esto 
+        // se crea un nuevo arreglo a partir del anterior utilizando la función map(). En su
+        // función de mapeo se solicita crear un nuevo objeto del tipo correcto a través de la
+        // función factoría. El nuevo arreglo obtenido se utiliza para crear el mapa.
+        return new Map(arreglo.map(([key, value]) => { 
+            return [key, funcionFactoria(value)];
+        }));
+    }
+
+    eliminar(key) {
+        localStorage.removeItem(key);
+    }
+}
+
 class Producto {
     constructor (id, nombre, precio) {
         this.id = id;
@@ -20,112 +54,170 @@ class Producto {
 }
 
 class Catalogo {
+    #productos = null;
+
     constructor () {
-        this.productos = new Map([]);
+        this.#leerMapa();
     }
 
-    buscarProductoPorNombre(nombre) {
-        // Obtener un arreglo de objetos de tipo Producto para poder utilizar find
-        // No existe función similar para Maps ni iteradores
-        const productos = Array.from(this.productos.values());
-        const producto = productos.find((producto) => { producto.nombre === nombre })
-        return producto.id ? producto !== undefined : null;
+    #guardarMapa () {
+        // Se almacena el mapa de productos. En este proceso se pierde el tipo de los objetos. 
+        AlmacenamientoLocal.guardarMapa(KEY_CATALOGO, this.#productos);
     }
 
-    adicionarProducto (nombre, precio) {
-        let id = buscarProductoPorNombre(nombre);
-        if (id !== null) {
+    #leerMapa () {
+        this.#productos = AlmacenamientoLocal.obtenerMapa(
+            KEY_CATALOGO, (object) => { return new Producto(...Object.values(object)); });
+    }
+
+    todos () {
+        // Devolver un arreglo de objetos para poder utilizar los métodos de Array.
+        // No están disponibles para iteradores.
+        return Array.from(this.#productos.values());
+    }
+
+    obtener (id) {
+        return this.#productos.get(id);
+    }
+
+    buscarPorNombre (nombre) {
+        const producto = this.todos().find((producto) => { return producto.nombre === nombre })
+        return producto !== undefined ? producto : null;
+    }
+
+    adicionar (nombre, precio) {
+        const producto = this.buscarPorNombre(nombre);
+
+        if (producto !== null) {
             // Se modifica el producto para evitar lanzar un error
-            this.modificarProducto(id, nombre, precio);
+            this.modificar(producto, nombre, precio);
+            return producto;
         }
-        else {
-            // Se utilizan el número de milisegundos del momento actual como el id
-            id = Date.now();
-            this.productos.set(id, new Producto(id, nombre.trim(), precio));
-        }
+
+        // Se crea un nuevo producto. Se utiliza el número de milisegundos 
+        // del momento actual como su id
+        const id = Date.now();
+        nuevoProducto = new Producto(id, nombre.trim(), parseFloat(precio))
+        this.#productos.set(id, nuevoProducto);
+        this.#guardarMapa();
+        return nuevoProducto;
     }
 
-    modificarProducto (id, nombre=null, precio=null) {
-        const producto = this.productos.get(id);
+    modificar (destino, nombre=null, precio=null) {
+        const producto = (destino instanceof Object) ? destino : this.obtener(destino);
+        // Si destino es null o no se encuentra el producto, se sale sin efectuar ninguna
+        // operación.
+        if (producto == undefined || producto == null) return;
         // Sólo se modifica el producto si se proporciona el nuevo nombre y/o precio
         if (nombre !== null) producto.nombre = nombre.trim();
         if (precio !== null) producto.precio = parseFloat(precio);
+        this.#guardarMapa();
     }
 
-    eliminarProducto (id) {
-        this.productos.delete(id);
+    eliminar (id) {
+        this.#productos.delete(id);
+        this.#guardarMapa();
     }
 }
 
 class CarritoLinea {
-    constructor (carrito, id, cantidad) {
-        this.carrito = carrito;
+    constructor (id, cantidad) {
         // id del producto
         this.id = id;
         // cantidad del producto
         this.cantidad = parseFloat(cantidad);
-        this.importe = 0.0;
-        this.calcularImporte();
-     }
+    }
 
-     calcularImporte() {
-        this.importe = this.carrito.catalogo.productos.get(this.id).precio * this.cantidad;
-     }
+    calcularImporte(catalogo) {
+        return catalogo.obtener(this.id).precio * this.cantidad;
+    }
 
-     aumentarCantidad(value) {
+    aumentarCantidad(value) {
         this.cantidad += parseFloat(value);
-        this.calcularImporte();
-     }
+    }
 
-     disminuirCantidad(value) {
+    disminuirCantidad(value) {
         this.cantidad -= parseFloat(value);
-        this.calcularImporte();
-     }
+    }
 }
 
 class Carrito {
+    #lineas = null;
+
     constructor(catalogo) {
         this.catalogo = catalogo;
-        this.lineas = new Map([]);
-        this.calculado = false;
         this.importeTotal = 0.0;
         this.importeTotalConIva = 0.0;
         this.importeTotalConDescuento = 0.0;
+        this.#leerMapa();
+        this.#calcularImportes();
+    }
+
+    #guardarMapa () {
+        // Se almacena el mapa de líneas. En este proceso se pierde el tipo de los objetos. 
+        AlmacenamientoLocal.guardarMapa(KEY_CARRITO, this.#lineas);
+    }
+
+    #leerMapa () {
+        this.#lineas = AlmacenamientoLocal.obtenerMapa(
+            KEY_CARRITO, (object) => { return new CarritoLinea(...Object.values(object)); });
+    }
+
+    #eliminarMapa () {
+        AlmacenamientoLocal.eliminar(KEY_CARRITO);
+    }
+
+    lineas () {
+        // Devolver un arreglo de objetos para poder utilizar los métodos de Array.
+        // No están disponibles para mapas ni para iteradores.
+        return Array.from(this.#lineas.values());
     }
 
     adicionarLinea (id, cantidad){
-        if (this.lineas.has(id)) {
+        if (this.#lineas.has(id)) {
             // Si el producto ya está en el carrito, se aumenta su cantidad para evitar lanzar error
-            this.lineas.get(id).aumentarCantidad(parseFloat(cantidad));
+            this.#lineas.get(id).aumentarCantidad(parseFloat(cantidad));
         } else {
-            this.lineas.set(id, new CarritoLinea(this, id, cantidad));
+            this.#lineas.set(id, new CarritoLinea(id, cantidad));
         }
-        this.calculado = false;
+        this.#calcularImportes();
+        this.#guardarMapa();
     }
 
     actualizarLinea (id, cantidad) {
         // Si el producto no está en el carrito no se hace nada, para evitar lanzar error
-        if (this.lineas.has(id)) {
-            this.lineas.get(id).cantidad = parseFloat(cantidad);
+        if (this.#lineas.has(id)) {
+            this.#lineas.get(id).cantidad = parseFloat(cantidad);
+            this.#calcularImportes();
+            this.#guardarMapa();
         }
     }
 
     eliminarLinea (id) {
-        this.lineas.delete(id);
+        this.#lineas.delete(id);
+        this.#calcularImportes();
+        this.#guardarMapa();
     }
 
-    calcularImporteTotal () {
+    vaciar () {
+        this.#lineas.clear();
+        this.#calcularImportes();
+        this.#eliminarMapa();
+    }
+
+    #calcularImporteTotal () {
         // Obtener un arreglo de objetos de tipo CarritoLinea para poder utilizar reduce
         // No existe función similar para Maps ni iteradores
-        const lineas = Array.from(this.lineas.values())
-        this.importeTotal = lineas.reduce((importeTotal, linea) => importeTotal + linea.importe, 0);
+        const lineas = Array.from(this.#lineas.values())
+        this.importeTotal = lineas.reduce(
+            (importeTotal, linea) => importeTotal + linea.calcularImporte(this.catalogo), 0);
     }
 
-    calcularImporteTotalConIva () {
+    #calcularImporteTotalConIva () {
         this.importeTotalConIva = this.importeTotal * (1 + IVA);
     }
 
-    calcularImporteTotalConDescuento () {
+    #calcularImporteTotalConDescuento () {
         if (this.importeTotalConIva < MONTO_MINIMO) {
             this.importeTotalConDescuento = this.importeTotalConIva;
         } else {
@@ -133,14 +225,10 @@ class Carrito {
         }
     }
 
-    calcularImportes () {
-        if (this.calculado) return;
-
-        this.calcularImporteTotal();
-        this.calcularImporteTotalConIva();
-        this.calcularImporteTotalConDescuento();
-
-        this.calculado = true;
+    #calcularImportes () {
+        this.#calcularImporteTotal();
+        this.#calcularImporteTotalConIva();
+        this.#calcularImporteTotalConDescuento();
     }
 
 }
@@ -152,12 +240,8 @@ class Impresora {
         // Construyendo la línea del encabezado de la factura
         let textoFactura = `${TITULO_PRODUCTO}\t${TITULO_CANTIDAD}\t${TITULO_PRECIO}\t${TITULO_SUBTOTAL}\n`;
 
-        // Obtener un arreglo de objetos de tipo CarritoLinea para poder utilizar reduce
-        // No existe función similar para Maps ni iteradores
-        const lineas = Array.from(this.lineas.values())
-
         // Construyendo las líneas de la factura
-        textoFactura = lineas.reduce(
+        textoFactura = this.carrito.lineas().reduce(
             (textoFactura, linea) => `${textoFactura}${this.imprimirFacturaLinea(carrito, linea)}`, textoFactura);
 
         // Construyendo la línea de separación
@@ -171,11 +255,6 @@ class Impresora {
         }
         textoFactura = Array.from(totales.entries()).reduce(
             ([titulo, valor]) => `${textoFactura}${this.imprimirTotalLinea(titulo, valor)}`, textoFactura);
-        // textoFactura += `\t\t\t\t\t\t${TITULO_IMPORTE}\t${carrito.importeTotal.toFixed(2)}\n`;
-        // textoFactura += `\t\t\t\t\t\t${TITULO_CON_IVA}\t${carrito.importeTotalConIva.toFixed(2)}\n`;
-        // textoFactura += `\t\t\t\t\t\t${TITULO_A_PAGAR}\t${carrito.importeTotalConDescuento.toFixed(2)}\n`;
-
-        console.log(textoFactura);
     }
 
     imprimirFacturaLinea (carrito, linea) {
